65311bf1d63b9db68d6ede8265a6624d61d8c31c
Abhishek Rathod <118195093+abhishek-rathod1@users.noreply.github.com>
2025-04-24 23:26:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // your code here
+const form = document.getElementById("infoForm")
+const link = document.getElementById("url");
+
+form.addEventListener("submit", function(e){
+	e.preventDefault();
+const name = document.getElementById("name").value.trim();
+const year = document.getElementById("year").value.trim();
+
+let baseUrl = `https://localhost:8080/`
+let params = [];
+
+if(name) params.push(`name=${name}`)
+if(year) params.push(`name=${year}`)
+// 
+link.textContent = params.length>0 ? `${baseUrl}?${params.join("&")}` : baseUrl;
+	
+})
